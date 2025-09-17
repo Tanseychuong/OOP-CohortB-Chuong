b36919e397cee6f6154426e0f3f9ba1e26d6cc95
Chuong T. N. Mayian <nyangchuongtiutiu@gmail.com>
2025-09-17 22:57:48 +0000

    it's it

---
+++ firstCode.java
@@ -0,0 +1,15 @@
+/*This is my first ever program in java and I already like java, 
+it's a cool programming language because it teach me to think and 
+apply my problem solving skills on it.*/
+import java.util.Scanner;
+public class firtCode{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextIn();
+        scanner.close();
+
+    }
+}
